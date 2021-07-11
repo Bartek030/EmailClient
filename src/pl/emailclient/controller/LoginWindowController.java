8e85f2f003c8b0882e61411e370b099fffdc7f3d
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-11 17:01:48 +0200

    Base controllers created

--- src/pl/emailclient/controller/LoginWindowController.java
+++ src/pl/emailclient/controller/LoginWindowController.java
@@ -4,8 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import pl.emailclient.EmailManager;
+import pl.emailclient.view.ViewFactory;
 
-public class LoginWindowController {
+public class LoginWindowController extends BaseController {
     @FXML
     private TextField emailAddressField;
 
@@ -14,6 +16,10 @@ public class LoginWindowController {
 
     @FXML
     private Label errorLabel;
+
+    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
 
     @FXML
     void loginButtonAction() {
---
+++ src/pl/emailclient/view/ViewFactory.java
@@ -0,0 +1,16 @@
+package pl.emailclient.view;
+
+import pl.emailclient.EmailManager;
+
+public class ViewFactory {
+
+    private EmailManager emailManager;
+
+    public ViewFactory(EmailManager emailManager) {
+        this.emailManager = emailManager;
+    }
+
+    public void showLoginWindow() {
+        System.out.println("show login window");
+    }
+}
