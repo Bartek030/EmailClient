c132ddf8adeb1759a86aa95b7e6474459bfac21c
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-11 17:38:17 +0200

    Stages initialized

--- src/pl/emailclient/view/ViewFactory.java
+++ src/pl/emailclient/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package pl.emailclient.view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import pl.emailclient.EmailManager;
+import pl.emailclient.controller.BaseController;
+import pl.emailclient.controller.LoginWindowController;
+
+import java.io.IOException;
 
 public class ViewFactory {
 
@@ -11,6 +19,21 @@ public class ViewFactory {
     }
 
     public void showLoginWindow() {
-        System.out.println("show login window");
+        BaseController controller = new LoginWindowController(emailManager, this, "loginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+
+        try {
+            parent = fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
