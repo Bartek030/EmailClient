af9df7be0aa3403e1f5be5b3591860add61d33d5
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-13 18:32:08 +0200

    Function fetching folders from email account added

--- src/pl/emailclient/EmailManager.java
+++ src/pl/emailclient/EmailManager.java
@@ -1,24 +1,24 @@
 package pl.emailclient;
 
 import javafx.scene.control.TreeItem;
+import pl.emailclient.controller.services.FetchFoldersService;
 import pl.emailclient.model.EmailAccount;
+import pl.emailclient.model.EmailTreeItem;
 
 public class EmailManager {
 
     // Folder handling:
-    private TreeItem<String> foldersRoot = new TreeItem<String>("");
+    private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
 
-    public TreeItem<String> getFoldersRoot() {
+    public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
     public void addEmailAccount(EmailAccount emailAccount) {
-        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
-        treeItem.setExpanded(true);
-            treeItem.getChildren().add(new TreeItem<String>("INBOX"));
-            treeItem.getChildren().add(new TreeItem<String>("Sent"));
-            treeItem.getChildren().add(new TreeItem<String>("Folder1"));
-            treeItem.getChildren().add(new TreeItem<String>("Spam"));
+        EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        fetchFoldersService.start();
+
         foldersRoot.getChildren().add(treeItem);
     }
 }
