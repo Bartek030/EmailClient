cf084b52ae239d8af8598a1fc739abe7c9b707d9
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-14 20:00:18 +0200

    Function receiving new email after the program start - added

--- src/pl/emailclient/EmailManager.java
+++ src/pl/emailclient/EmailManager.java
@@ -2,11 +2,17 @@ package pl.emailclient;
 
 import javafx.scene.control.TreeItem;
 import pl.emailclient.controller.services.FetchFoldersService;
+import pl.emailclient.controller.services.FolderUpdaterService;
 import pl.emailclient.model.EmailAccount;
 import pl.emailclient.model.EmailTreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
 
+    private FolderUpdaterService folderUpdaterService;
     // Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
 
@@ -14,9 +20,20 @@ public class EmailManager {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
+    public List<Folder> getFolderList() {
+        return folderList;
+    }
+
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, folderList);
         fetchFoldersService.start();
 
         foldersRoot.getChildren().add(treeItem);
