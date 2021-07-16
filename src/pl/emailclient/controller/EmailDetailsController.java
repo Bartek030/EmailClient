37030632ffd9c00018560ec2159e3ec2b2778a7e
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-07-16 20:38:36 +0200

    Downloading and opening attachments

--- src/pl/emailclient/controller/EmailDetailsController.java
+++ src/pl/emailclient/controller/EmailDetailsController.java
@@ -1,7 +1,10 @@
 package pl.emailclient.controller;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.web.WebView;
@@ -10,10 +13,17 @@ import pl.emailclient.controller.services.MessageRendererService;
 import pl.emailclient.model.EmailMessage;
 import pl.emailclient.view.ViewFactory;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import java.awt.Desktop;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EmailDetailsController extends BaseController implements Initializable {
+
+    private String LOCATION_OF_DOWNLOADS = System.getProperty("user.home") + "/Downloads/";
+
     @FXML
     private WebView webView;
 
@@ -39,9 +49,77 @@ public class EmailDetailsController extends BaseController implements Initializa
         EmailMessage emailMessage = emailManager.getSelectedMessage();
         subjectLabel.setText(emailMessage.getSubject());
         senderLabel.setText(emailMessage.getSender());
+        loadAttachments(emailMessage);
 
         MessageRendererService messageRendererService = new MessageRendererService(webView.getEngine());
         messageRendererService.setEmailMessage(emailMessage);
         messageRendererService.restart();
+    }
+
+    private void loadAttachments(EmailMessage emailMessage) {
+        if(emailMessage.hasAttachments()) {
+            for(MimeBodyPart mimeBodyPart: emailMessage.getAttachmentList()) {
+                try {
+                    AttachmentButton button = new AttachmentButton(mimeBodyPart);
+                    hBoxDownloads.getChildren().add(button);
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            attachmentLabel.setText("");
+        }
+    }
+
+    private class AttachmentButton extends Button {
+
+        private MimeBodyPart mimeBodyPart;
+        private String dowloadedFilePath;
+
+        public AttachmentButton (MimeBodyPart mimeBodyPart) throws MessagingException {
+            this.mimeBodyPart = mimeBodyPart;
+            this.setText(mimeBodyPart.getFileName());
+            this.dowloadedFilePath = LOCATION_OF_DOWNLOADS + mimeBodyPart.getFileName();
+
+            this.setOnAction(e -> downloadAttachment());
+        }
+
+        private void downloadAttachment(){
+            colorBlue();
+            Service service = new Service() {
+                @Override
+                protected Task createTask() {
+                    return new Task() {
+                        @Override
+                        protected Object call() throws Exception {
+                            mimeBodyPart.saveFile(dowloadedFilePath);
+                            return null;
+                        }
+                    };
+                }
+            };
+            service.restart();
+            service.setOnSucceeded(e -> {
+                colorGreen();
+                this.setOnAction(e2 -> {
+                    File file = new File(dowloadedFilePath);
+                    Desktop desktop = Desktop.getDesktop();
+                    if(file.exists()) {
+                        try {
+                            desktop.open(file);
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+            });
+        }
+
+        private void colorBlue() {
+            this.setStyle("-fx-background-color: Blue");
+        }
+        private void colorGreen() {
+            this.setStyle("-fx-background-color: Green");
+        }
     }
 }
